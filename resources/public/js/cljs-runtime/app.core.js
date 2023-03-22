5bcbd70ef4cd050ec3dd74b562600ecd25d9200b
Ahmad Rauhan <52809106+ohanlines@users.noreply.github.com>
2023-03-22 23:54:55 +0700

    Delete resources/public/js directory

--- resources/public/js/cljs-runtime/app.core.js
+++
@@ -1,9 +0,0 @@
-goog.provide('app.core');
-app.core.init = (function app$core$init(){
-console.log("HALO 1");
-
-return console.log("HALO 2");
-});
-goog.exportSymbol('app.core.init', app.core.init);
-
-//# sourceMappingURL=app.core.js.map
