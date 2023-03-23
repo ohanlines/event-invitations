d480f5b483a83a617af9df38d78677f259829fd8
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-03-23 22:15:32 +0700

    trying helix

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,9 +1,18 @@
 (ns app.core
-  (:require [ajax.core :refer [GET]]))
+  (:require [ajax.core :refer [GET]]
+            [helix.core :refer [defnc $]]
+            [helix.dom :as d]
+            ["react-dom/client" :as dom]))
+
+(defnc app []
+  (d/div "COBA 123"))
 
 (defn ^:export init []
-  (js/console.log "HALO 1")
-  (js/console.log "HALO 2"))
+  (let [root (dom/createRoot (js/document.getElementById "app"))]
+    (.render root ($ app))
+    (js/console.log "HALO 1")
+    (js/console.log "HALO 2"))
+  )
 
 (comment
 
