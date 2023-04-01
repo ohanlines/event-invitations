c346b034df7cf687a888b546cc5e38017ccb1284
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-04-01 23:05:59 +0700

    try using heroicon lib

--- src/cljs/app/views/home.cljs
+++ src/cljs/app/views/home.cljs
@@ -1,5 +1,5 @@
 (ns app.views.home
-  )
+  (:require ["@heroicons/react/24/solid" :refer [BeakerIcon]]))
 
 (defn home-page []
   [:div
@@ -11,5 +11,20 @@
      [:img {:src "/img/img-2.jpg" :alt "IMG-2"}]]
     [:div {:class-name "w-64 flex-shrink-0"}
      [:img {:src "/img/img-3.jpg" :alt "IMG-3"}]]
+
+    [:div {:class-name "card"}
+     [:p "Asti"]]
+
+    [:div {:class-name "card"}
+     [:p "test"]
+     [:> BeakerIcon {:class-name "h-6 w-6 text-blue-500"}]
+     ]
     ]
+
+   [:div {:class-name "w-64 grid grid-cols-1"}
+    [:div {:class-name "card p-0 relative overflow-hidden"}
+     [:img {:class-name "object-cover transition-opacity duration-150 ease-in-out"
+            :src        "/img/img-1.jpg"}]]]
+
+
    ])
