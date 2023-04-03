c72c812feea0db6530b57a26582e457f06f3e86b
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-04-03 18:41:11 +0700

    enable scroll snap

--- src/cljs/app/views/home.cljs
+++ src/cljs/app/views/home.cljs
@@ -9,12 +9,18 @@
     ["img-1" "img-2" "img-3"]))
 
 (defn home-page []
-  [:div
-   [:p "Welcome Home"]
+  [:div {:class-name "snap-y snap-mandatory bg-gradient-to-r from-purple-300 to-pink-100 w-screen h-screen overflow-scroll"}
 
-   [:div {:class-name "max-w-xs relative group"}
-    [:div {:class-name "rounded-lg relative overflow-hidden"}
-     [:img {:src (images @image-idx)}]]
+   ;; === WELCOME SPEECH =========================
+   [:div {:class-name "snap-start w-screen h-screen flex items-center justify-center"}
+    [:p "Welcome Speech"]]
+
+   ;; === PHOTO SESSION ==========================
+   [:div {:class-name "snap-start w-screen h-screen flex items-center justify-center"}
+
+    [:div {:class-name "max-w-xs relative group"}
+     [:div {:class-name "rounded-lg relative overflow-hidden"}
+      [:img {:src (images @image-idx)}]]
 
     ;; left arrow
     (if (not= @image-idx 0)
@@ -26,8 +32,23 @@
     (if (not= @image-idx 2)
       [:div {:class-name "hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 cursor-pointer"
              :on-click   #(swap! image-idx inc)}
-       [:> ChevronRightIcon {:class-name "h-6 w-6 text-white"}]])]
+       [:> ChevronRightIcon {:class-name "h-6 w-6 text-white"}]])]]
 
+   ;; === D-DAY COUNTDOWN =======================
+   [:div {:class-name "snap-start w-screen h-screen flex items-center justify-center"}
+    [:p "D-day Countdown"]]
+
+   ;; === EVENT LOCATION =========================
+   [:div {:class-name "snap-start w-screen h-screen flex items-center justify-center"}
+    [:p "Event Location"]]
+
+   ;; === ATTENDANCE FORM ========================
+   [:div {:class-name "snap-start w-screen h-screen flex items-center justify-center"}
+    [:p "Attendance Form"]]
+
+   ;; === COMMENT FORM AND DISPLAY ===============
+   [:div {:class-name "snap-start w-screen h-screen flex items-center justify-center"}
+    [:p "Comment Form and Display"]]
 
    ])
 
