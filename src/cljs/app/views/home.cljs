5d9a96d7220b4d888feaed12046ef2da9ef38e97
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-04-03 17:01:31 +0700

    conditional image slider

--- src/cljs/app/views/home.cljs
+++ src/cljs/app/views/home.cljs
@@ -4,8 +4,6 @@
 
 (def image-idx (r/atom 0))
 
-(set-validator! image-idx #(and (>= @image-idx 0) (<= @image-idx 2)))
-
 (def images
   (mapv #(str "/img/" % ".jpg")
     ["img-1" "img-2" "img-3"]))
@@ -15,20 +13,20 @@
    [:p "Welcome Home"]
 
    [:div {:class-name "max-w-xs relative group"}
-    [:div {:class-name "card p-0 relative overflow-hidden"}
-     [:img {:class-name "object-cover transition-opacity duration-150 ease-in-out"
-            :src        (images @image-idx)}]]
+    [:div {:class-name "rounded-lg relative overflow-hidden"}
+     [:img {:src (images @image-idx)}]]
 
     ;; left arrow
-    [:div {:class-name "hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
-           :on-click   #(swap! image-idx dec)}
-     [:> ChevronLeftIcon {:class-name "h-6 w-6 text-white"}]]
+    (if (not= @image-idx 0)
+      [:div {:class-name "hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 cursor-pointer"
+             :on-click   #(swap! image-idx dec)}
+       [:> ChevronLeftIcon {:class-name "h-6 w-6 text-white"}]])
 
-    (js/console.log "IMG IDX: " @image-idx)
     ;; right arrow
-    [:div {:class-name "hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
-           :on-click   #(swap! image-idx inc)}
-     [:> ChevronRightIcon {:class-name "h-6 w-6 text-white"}]]]
+    (if (not= @image-idx 2)
+      [:div {:class-name "hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 cursor-pointer"
+             :on-click   #(swap! image-idx inc)}
+       [:> ChevronRightIcon {:class-name "h-6 w-6 text-white"}]])]
 
 
    ])
