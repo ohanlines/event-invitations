c6b176a6af5c82b6e1d86ea6b30068eff5ebe507
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-04-15 15:50:53 +0700

    add databases library

--- project.clj
+++ project.clj
@@ -4,16 +4,27 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+
+                 ;; server routing
                  [io.pedestal/pedestal.service "0.5.7"]
                  [io.pedestal/pedestal.route "0.5.7"]
                  [io.pedestal/pedestal.jetty "0.5.7"]
                  [com.stuartsierra/component "0.4.0"]
                  [org.slf4j/slf4j-simple "1.7.28"]
                  [com.stuartsierra/component.repl "0.2.0"]
+
+                 ;; client routing
                  [reagent "1.2.0"]
                  [metosin/reitit "0.6.0"]
                  [metosin/reitit-spec "0.6.0"]
                  [metosin/reitit-frontend "0.6.0"]
+
+                 ;; database
+                 [com.github.seancorfield/next.jdbc "1.3.865"]
+                 [com.github.seancorfield/honeysql "2.4.1011"]
+                 [mysql/mysql-connector-java "8.0.32"]
+
+                 ;; json
                  [cheshire "5.11.0"]]
   ;; :main ^:skip-aot event-invitations.core
   :target-path "target/%s"
